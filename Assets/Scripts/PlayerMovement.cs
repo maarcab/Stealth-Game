7fd6d41fa3e50471c943b4cea6abb268722ed9c6
maarcab <maragonesb@edu.tecnocampus.cat>
2026-02-11 22:26:23 +0100

    Player animation fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,35 +8,27 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 inputVector;
     private bool isMoving;
-    private Animator animator;
 
     public bool IsMoving => isMoving;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
     public void OnMove(InputValue value)
     {
         inputVector = value.Get<Vector2>();
+        isMoving = inputVector.sqrMagnitude > 0.01f;
     }
+
     private void FixedUpdate()
     {
         rb.linearVelocity = inputVector * speed;
-        isMoving = inputVector.sqrMagnitude > 0.01f;
-
-        if (isMoving)
-        {
-           
-
-            animator.SetFloat("MoveX", inputVector.x);
-            animator.SetFloat("MoveY", inputVector.y);
-        }
     }
+
     public Vector2 GetMoveInput()
     {
-        return rb.linearVelocity.normalized;
+        return inputVector;
     }
 }
